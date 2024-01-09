3cf099ed1e569c5ee093e08402f7638d61c21328
aamir <aamirkhan@engineer.com>
2024-01-09 18:52:10 +0500

    replaced mapping to array in contract but not deployed yet

---
+++ hardhat/contracts/MarketplaceV2.sol
@@ -0,0 +1,96 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+contract Marketplace {
+
+    struct NftStructure {
+        uint itemId;
+        uint tokenId;
+        address seller;
+        address owner;
+        address nft;
+        uint price;
+        bool isSold;
+    }
+
+    NftStructure[] public nftCollection;
+    uint public currentNft;
+
+    address public immutable owner;
+
+
+    // event for new purchase
+    event ItemSold(
+        address item,
+        address buyer,
+        address seller,
+        uint price
+    );
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+
+    function purchase(uint itemId) external payable {
+
+        NftStructure memory item = nftCollection[itemId];
+        
+        require(item.nft != address(0), "This item not exists");
+
+        require(item.price <= msg.value, "Not enough amount to purchase");
+        require(item.isSold == false, "This item is already sold");
+        // uint feePrice = calculateFeePrice(item.price);
+
+        IERC721(item.nft).transferFrom(item.seller, msg.sender, item.tokenId); 
+
+        // change the NFT details
+        item.isSold = true;
+        item.seller = item.owner;
+        item.owner = msg.sender;
+
+        // payable(item.seller).transfer(item.price - feePrice);
+        // payable(owner).transfer(feePrice);
+
+        nftCollection[itemId] = item;
+
+        payable(item.seller).transfer(item.price);
+
+
+        // emits the SoldItem event
+        emit ItemSold(
+            item.nft,
+            item.owner,
+            item.seller,
+            item.price
+        );
+    }
+
+    function listItem(address nft, uint tokenId, uint price) public {
+        
+        // price is not 0
+        // contract is approved to transfer the token.
+        // increment the totalListed var.
+        // save the record.
+
+        require(price > 0, "shold greater then 0");
+        require(IERC721(nft).getApproved(tokenId) == address(this), "First send the NFT to Contract.");
+
+        nftCollection.push(NftStructure(
+            currentNft,
+            tokenId,
+            msg.sender,
+            msg.sender,
+            nft,
+            price,
+            false
+        ));
+
+        // increment the currentNft
+        unchecked {
+            currentNft++;
+        }
+    }
+
+}
